571a22e4b95f12edf8a2ca52d1f0b80e15b13e03
Stefano Gini <61376940+Haruno19@users.noreply.github.com>
2022-04-05 13:51:50 +0200

    Add files via upload

---
+++ starfetch/install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+INSTALL_DIR='/usr/local/'
+BIN_DIR='/usr/local/bin'
+
+rm -rf $INSTALL_DIR/starfetch || return 1
+
+mkdir -p $INSTALL_DIR/starfetch || return 1
+
+cp -rf ./constellations $INSTALL_DIR/starfetch
+cp -rf ./res $INSTALL_DIR/starfetch
+g++ ./src/starfetch.cpp -o $INSTALL_DIR/starfetch/starfetch
+
+rm -rf $BIN_DIR/starfetch || return 1
+ln -s $INSTALL_DIR/starfetch/starfetch $BIN_DIR/starfetch
