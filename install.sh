848557ec8a2b6384d8184aabe4636f02928301d5
Stefano Gini <61376940+Haruno19@users.noreply.github.com>
2022-04-06 10:24:18 +0200

    Added g++ flags to match the latest code requirements

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ rm -rf $INSTALL_DIR/starfetch || return 1
 mkdir -p $INSTALL_DIR/starfetch || return 1
 
 cp -rf ./res $INSTALL_DIR/starfetch
-g++ ./src/starfetch.cpp -o $INSTALL_DIR/starfetch/starfetch
+g++ -std=c++17 ./src/starfetch.cpp -o $INSTALL_DIR/starfetch/starfetch
 
 rm -rf $BIN_DIR/starfetch || return 1
 ln -s $INSTALL_DIR/starfetch/starfetch $BIN_DIR/starfetch
